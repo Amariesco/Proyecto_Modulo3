9577e47fa7077dbf3724866df97c909f83f777f6
Ana Marión Escobar <ana.marion.esc@gmail.com>
2026-01-29 17:54:28 -0300

    Se asigna comentario para definir carpeta modulos.

--- leccion_01/Modulos/__init__.py
+++ leccion_01/Modulos/__init__.py
@@ -1,2 +1,6 @@
 # Este archivo es necesario para que Python trate este directorio como un paquete.
 # Se deja vacío porque no se requiere inicialización específica por el momento.
+"""
+Paquete de módulos del sistema.
+Contendra la lógica para gestionar de datos, menús y validaciones.
+"""
